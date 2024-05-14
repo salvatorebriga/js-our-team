b00e1e98858683b6a33783f6c4e97983a6baab0f
salvatorebriga <salvo.z2302@gmail.com>
2024-05-14 16:43:34 +0200

    Milestone 1

--- main.js
+++ main.js
@@ -2,38 +2,47 @@
 const employe = [
 
     employe1 = {
-        nome: 'Wayne Barnett',
-        ruole: 'Founder & CEO',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
         image: 'wayne-barnett-founder-ceo.jpg'
     },
 
     employe2 = {
-        nome: 'Angela Caroll',
-        ruole: 'Chief Editor',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
         image: 'angela-caroli-chief-editor.jpg'
     },
 
     employe3 = {
-        nome: 'Walter Gordon',
-        ruole: 'Office Manager',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
         image: 'walter-gordon-office-manager.jpg'
     },
 
     employe4 = {
-        nome: 'Angela Lopez',
-        ruole: 'Social Media Manager',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
         image: 'angela-lopez-social-media-manager.jpg'
     },
 
     employe5 = {
-        nome: 'Scott Estrada',
-        ruole: 'Developer',
+        name: 'Scott Estrada',
+        role: 'Developer',
         image: 'scott-estrada-developer.jpg'
     },
 
     employe6 = {
-        nome: 'Barbara Ramos',
-        ruole: 'Graphic Designer',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
         image: 'barbara-ramos-graphic-designer.jpg'
     }
 ];
+
+/*Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto*/
+
+for(let i = 0; i < employe.length; i++){
+    console.log('%cMembro del Team numero: ' + (i + 1) , 'color: #FF0000');
+    console.log('Nome: ' + employe[i].name);
+    console.log('Ruolo: ' + employe[i].role);
+    console.log('Link alla foto: ' + employe[i].image);
+}
