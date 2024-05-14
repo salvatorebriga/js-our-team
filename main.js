92bf374200cb73624ff65eadf0b12eec993cc880
salvatorebriga <salvo.z2302@gmail.com>
2024-05-14 16:24:40 +0200

    Milestone 0

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
-"use strict"
+const employe = [
 
+    employe1 = {
+        nome: 'Wayne Barnett',
+        ruole: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    employe2 = {
+        nome: 'Angela Caroll',
+        ruole: 'Chief Editor',
+        image: 'angela-caroli-chief-editor.jpg'
+    },
+
+    employe3 = {
+        nome: 'Walter Gordon',
+        ruole: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+
+    employe4 = {
+        nome: 'Angela Lopez',
+        ruole: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    employe5 = {
+        nome: 'Scott Estrada',
+        ruole: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+
+    employe6 = {
+        nome: 'Barbara Ramos',
+        ruole: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
